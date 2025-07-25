81438a37bc68b56e02050090ad7ffeedde267267
Victor Simões <105655697+vittodip@users.noreply.github.com>
2025-07-25 00:31:22 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-    return <h1>O curso.dev realmente é bom, recomendo</h1>
+    return (
+        <div>
+            <h1>O curso.dev é bom...</h1>
+            <hr/>
+            <h6>❤️ Só não é tão bom quanto ter você aqui comigo ❤️</h6>
+            <hr/>
+            <hr/>
+            <hr/>
+            <p>tubiraum daum</p>
+            <p>tubiraum daum</p>
+            <hr/>
+            <hr/>
+            <hr/>
+            <h2>Eu te amo amor</h2>
+        </div>
+    )
 }
 
 export default Home;
