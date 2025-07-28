bd8d85f33fde88a4014dd0b441eea989a30a3ac0
Victor Simões <105655697+vittodip@users.noreply.github.com>
2025-07-28 01:45:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 function Home() {
-    return (
-        <div>
-            <h1>O curso.dev é bom...</h1>
-            <hr/>
-            <h3>❤️ Só não é tão bom quanto ter você aqui comigo ❤️</h3>
-            <br/>
-            <br/>
-            <br/>
-            <p>tubiraum daum</p>
-            <p>tubiraum daum</p>
-            <br/>
-            <br/>
-            <br/>
-            <h2>Eu te amo amor</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h1>O curso.dev é bom...</h1>
+      <hr />
+      <h3>❤️ Só não é tão bom quanto ter você aqui comigo ❤️</h3>
+      <br />
+      <br />
+      <br />
+      <p>tubiraum daum</p>
+      <p>tubiraum daum</p>
+      <br />
+      <br />
+      <br />
+      <h2>Eu te amo amor</h2>
+    </div>
+  );
 }
 
 export default Home;
