fb2a2669bee1d25481a2cda604f496155a3e23e4
Victor Simões <105655697+vittodip@users.noreply.github.com>
2025-09-03 01:03:43 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,5 +1,4 @@
 const { exec } = require("node:child_process");
-const { stdout } = require("node:process");
 
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
